5fc8a8485885f9983f1449ed62ab9924ee27093f
ГАМШИГ <156410124+QuizGames156@users.noreply.github.com>
2026-09-04 12:03:21 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gamshig4-v8-exam-result-overlay';
+const CACHE='gamshig4-v9-exam-rootfix';
 const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
